7f974fd14c840cf2159a542b24421d5bd36ec100
manuel1358000 <manuel13580@gmail.com>
2019-08-13 19:59:25 -0600

    Se agrego la comunicacion entre servicio de solicitud de clientes y la recepcion de solicitudes por medio del ESB

--- ESB/esb.js
+++ ESB/esb.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
+var request=require('request');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.post('/solicitud', function (req, res) {
-    console.log(req.body['nombre']);
     let respuesta='';
-if(req.body['nombre']!=null) {
+    Enviar_Solicitud_Recepcion(req.body).then(function(respuesta){
+        res.send(respuesta);
+    });
+
+
+
+/*
+    if(req.body['nombre']!=null) {
         console.log('Se completo la solicitud para '+req.body.nombre);
         respuesta='Solicitud Completa '+req.body['nombre'];
     }else{
         console.log('No se completo la solicitud');
         respuesta='Solicitud Incompleta';
     }
-    res.send(respuesta);
+    res.send(respuesta);*/
 });
+
+
 
 app.post('/solicitud_piloto', function (req, res) {
     res.send('Solicitud Piloto Completa');
@@ -23,3 +32,27 @@ app.post('/solicitud_piloto', function (req, res) {
 app.listen(3000, () => {
  console.log("El ESB esta en el puerto 3000");
 });
+
+function Enviar_Solicitud_Recepcion(json){
+    const promise=new Promise(function(resolve,reject){    
+        var jsonDataObj =json;
+        //se consume el servicio por medio de un request POST, en el cual le enviamos en el body el json con la informacion del cliente
+        request({
+            method: "POST",
+            url:'http://localhost:3001/recepcion_solicitudes',
+            json: true,
+            body: jsonDataObj
+        },function(error,response,body){
+            //si ocurre un error al solicitar la respuesta, se muestra la alerta
+            if(error!=null){
+                reject('Ocurrio un Error reject');
+            }else{
+                resolve(body);    
+            }
+        });
+    });
+    return promise;
+}
+
+
+
--- Microservicios/s_solicitud.js
+++ Microservicios/s_solicitud.js
@@ -5,19 +5,18 @@ function obtenerNombre(){
     const promise=new Promise(function(resolve,reject){
         console.log('Ingrese Nombre');
         var stdin=process.openStdin();
-        stdin.addListener('data',function(nombre){
-            //retornamos la informacion capturada y se la enviamos por medio de una promesa
-            resolve(nombre.toString().trim());
+        stdin.addListener('data',function(data){
+            //retornamos la informacion capturada, la convertimos a string y le quitamos los caracteres de salto de linea
+            resolve(data.toString().trim());
             }
         );
     });
     return promise;
 }
-
 //funcion que consume el servicio para la solicitud de un UBER
 function enviarSolicitud(nombre){
     //json que contiene el nombre de la persona solicitante
-    var jsonDataObj = {'nombre': nombre};
+    var jsonDataObj = {'nombre': nombre,'id':Math.floor(Math.random() * (1500 -100)) + 100};
     //se consume el servicio por medio de un request POST, en el cual le enviamos en el body el json con la informacion del cliente
     request({
         method: "POST",
@@ -25,8 +24,16 @@ function enviarSolicitud(nombre){
         json: true,
         body: jsonDataObj
     },function(error,response,body){
-        //en esta parte nos retorna la respuesta del servicio
-        console.log(body);
+        //si ocurre un error al solicitar la respuesta, se muestra la alerta
+        if(error!=null){
+            console.log('Ocurrio un error inesperado, el servicio no esta disponible, intente nuevamente');
+            //finalizamos el servicio para que ingrese una nueva solicitud el usuario
+            process.exit(1);
+        }else{
+            //en esta parte nos retorna la respuesta del servicio
+            console.log(body);
+            process.exit(1);    
+        }
     });
 }
 
